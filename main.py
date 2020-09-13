9a42fe4896ba88c24afa1084491ca859ba9bba7f
Bread <civisimulator@gmail.com>
2020-09-12 20:24:37 -0400

    replace magic numbers with variables

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import pygame
 from pygame.locals import K_ESCAPE, KEYDOWN, QUIT, K_w, K_s, K_a, K_d
 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
 class Player():
     def __init__(self):
         self.surf = pygame.Surface((100, 40))
         self.surf.fill(pygame.Color('blue'))
         self.rect = self.surf.get_rect()
+        self.speed = 5
+
     def render(self, screen):
         screen.blit(self.surf, self.rect)
+
     def handle_event(self, event):
         pass
+
     def update(self, keys):
         if keys[K_w]:
-            self.rect.move_ip(0,-5)
+            self.rect.move_ip(0, -self.speed)
         elif keys[K_s]:
-            self.rect.move_ip(0,5)
+            self.rect.move_ip(0, self.speed)
         elif keys[K_a]:
-            self.rect.move_ip(-5,0)
+            self.rect.move_ip(-self.speed, 0)
         elif keys[K_d]:
-            self.rect.move_ip(5,0)
+            self.rect.move_ip(self.speed, 0)
 
         if self.rect.left < 0:
             self.rect.left = 0
-        elif self.rect.right > 640:
-            self.rect.right = 640
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
         elif self.rect.top < 0:
             self.rect.top = 0
-        elif self.rect.bottom > 480:
-            self.rect.bottom = 480
+        elif self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
 
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 screen.fill(pygame.Color('lightgrey'))
 clock = pygame.time.Clock()
 
