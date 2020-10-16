8abfd5459f246bd388678d8cd58b9873e75b8df1
Bread <civisimulator@gmail.com>
2020-10-15 20:33:00 -0400

    add scoring display

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 import pygame
 from pygame.locals import K_ESCAPE, KEYDOWN, QUIT, K_w, K_s, K_a, K_d, K_SPACE, K_LEFT, K_RIGHT, RLEACCEL
+from pygame.math import Vector2
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
@@ -13,6 +14,22 @@ def hitbox_collision(player, entity):
     if (rect.x + rect.width) > hitbox[0] and rect.x < (hitbox[0] + hitbox[2]):
         if (rect.y + rect.height) > hitbox[1] and rect.y < (hitbox[1] + hitbox[3]):
             return True
+
+class HUD():
+    def __init__(self):
+        self.font = pygame.font.Font(None, 30)
+        self.color = pygame.Color('black')
+        self.label = self.font.render("Score: ", True, self.color)
+        self.position = Vector2(20, 20)
+        self.last_tick = pygame.time.get_ticks()
+
+    def render(self, screen):
+        screen.blit(self.label, self.position)
+        screen.blit(self.score, (self.position.x + self.label.get_width() + 5, self.position.y))
+
+    def update(self):
+        self.last_tick = pygame.time.get_ticks()
+        self.score = self.font.render(str(self.last_tick), True, self.color)
 
 class Raindrop(pygame.sprite.Sprite):
     def __init__(self):
@@ -127,6 +144,7 @@ obstacles.add(puddle)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 all_sprites.add(puddle)
+hud = HUD()
 
 running = True
 while running:
@@ -146,10 +164,12 @@ while running:
     player.update()
     puddle.update()
     raindrops.update()
+    hud.update()
 
     screen.fill(pygame.Color('lightgrey'))
     for entity in all_sprites:
         entity.render(screen)
+    hud.render(screen)
 
     pygame.display.flip()
 
